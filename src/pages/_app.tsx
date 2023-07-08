5713d2d0734cdcdb75a1e8b76f12c034e07936cc
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2023-07-09 01:16:49 +0300

    configs for graphql and codegen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app'
 import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout'
 import '~/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import client from '../../apolloClient'
 
 const App = ({ Component, pageProps }: AppProps) => (
-    <DefaultLayout>
-        <Component {...pageProps} />
-    </DefaultLayout>
+	<ApolloProvider client={client}>
+		<DefaultLayout>
+			<Component {...pageProps} />
+		</DefaultLayout>
+	</ApolloProvider>
 )
 
 export default App
