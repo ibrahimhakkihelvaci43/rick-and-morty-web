1a6cb34ebe7e7c460725c0025ef210167f3aa5f5
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2023-07-09 18:06:00 +0300

    add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import DefaultLayout from '~/layouts/DefaultLayout/DefaultLayout'
 import '~/styles/globals.css'
 import { ApolloProvider } from '@apollo/client'
 import client from '../../apollo-client'
+import GlobalStyle from '../globalStyles'
 
 const App = ({ Component, pageProps }: AppProps) => (
 	<ApolloProvider client={client}>
+		<GlobalStyle />
 		<DefaultLayout>
 			<Component {...pageProps} />
 		</DefaultLayout>
