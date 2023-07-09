cdd25dd3ff83cea5ec81a34b99d55ae42f90b37c
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2023-07-10 02:03:51 +0300

    add test for characters page

---
+++ src/pages/characters/characters.test.tsx
@@ -0,0 +1,43 @@
+import { getPage } from 'next-page-tester'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+it('renders locatons page with data', async () => {
+	const { render } = await getPage({
+		route: '/',
+	})
+
+	render()
+
+	expect(screen.getAllByAltText('character-image')).toHaveLength(20)
+})
+
+it('fetches the filtered data', async () => {
+	const { render } = await getPage({
+		route: '/',
+	})
+
+	render()
+
+	await userEvent.click(screen.getByRole('button', { name: /ADVANCED FILTERS/i }))
+	await userEvent.type(screen.getByPlaceholderText(/species/i), 'Human')
+	await userEvent.click(screen.getByText(/gender/i))
+	await userEvent.click(screen.getByText('Male'))
+	await userEvent.click(screen.getByText(/status/i))
+	await userEvent.click(screen.getByText(/alive/i))
+	await userEvent.click(screen.getByRole('button', { name: /APPLY/i }))
+
+	expect(screen.getAllByAltText('character-image')).toHaveLength(20)
+})
+
+it('filters by name', async () => {
+	const { render } = await getPage({
+		route: '/',
+	})
+
+	render()
+
+	await userEvent.type(screen.getByPlaceholderText(/filter by name.../i), 'Morty Smith')
+
+	expect(screen.getAllByAltText('character-image')).toHaveLength(1)
+})
