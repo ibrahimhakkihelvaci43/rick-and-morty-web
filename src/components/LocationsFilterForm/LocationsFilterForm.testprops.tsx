327c7186593b004cb3fc5c9c0e3b3c663fa09765
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2023-07-10 10:19:47 +0300

    fix type errors

--- src/components/LocationsFilterForm/LocationsFilterForm.testprops.tsx
+++ src/components/LocationsFilterForm/LocationsFilterForm.testprops.tsx
@@ -1,26 +1,6 @@
 import { ILocationsFilterForm } from './LocationsFilterForm.types'
 
 const LocationsFilterFormProps: ILocationsFilterForm = {
-	species: [
-		{
-			label: 'Human',
-			value: 'human',
-		},
-		{
-			label: 'Alien',
-			value: 'alien',
-		},
-	],
-	dimensions: [
-		{
-			label: 'Dimension C-137',
-			value: 'Dimension C-137',
-		},
-		{
-			label: 'Post-Apocalyptic Dimension',
-			value: 'Post-Apocalyptic Dimension',
-		},
-	],
 	onSubmit: (data) => {
 		console.log(data)
 	},
